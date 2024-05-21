776b5b748717b53466974613e13bdcc7be65193c
mtphuc253 <mtp25032003@gmail.com>
2024-05-21 07:58:53 +0700

    fix:log  warning

--- src/Services/axios/config.jsx
+++ src/Services/axios/config.jsx
@@ -10,7 +10,7 @@ const axiosClient = axios.create({
 // Add a request interceptor
 axiosClient.interceptors.request.use(
   function (config) {
-    const token = sessionStorage.getItem('token');
+    const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
       config.headers.Accept = 'application/json';
