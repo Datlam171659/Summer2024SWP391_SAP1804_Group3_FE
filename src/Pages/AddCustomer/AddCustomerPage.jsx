7714c8227e71ad6f6bf05a15f02c9816c54e629e
timboace0 <khanglgse171640@fpt.edu.vn>
2024-05-29 13:56:22 +0700

    Update AddCustomer UI

--- src/Pages/AddCustomer/AddCustomerPage.jsx
+++ src/Pages/AddCustomer/AddCustomerPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import {LeftOutlined} from '@ant-design/icons';
-import { Button, Form, Input, Typography } from 'antd'
+import { Button, Form, Input, Typography, Radio, message, Modal } from 'antd'
 import "./AddCustomerPage.scss"
 import CustomerApi from "../../Services/api/CustomerApi";
 
@@ -12,12 +12,49 @@ const AddCustomerPage = () => {
 
     const onFinish = async (values) => {
       try {
-        await CustomerApi.addCustomer(values);
-        navigate(-1);
-      } catch (error) {
-        console.log('Failed to add customer:', error);
-      }
-    };
+        // Check if the email already exists
+          let emailExists = await CustomerApi.getCustomerByEmail(values.email);
+          if (emailExists.length > 0) {
+              Modal.warning({
+                  title: "Error",
+                  content: `Email already in use, please use a different email`,
+                  okText: "OK"
+              });
+              return;
+          }
+
+          // Check if the phone number already exists
+          let phoneNumberExists = await CustomerApi.getCustomerByPhoneNumber(values.phoneNumber);
+          if (phoneNumberExists.length > 0) {
+              Modal.warning({
+                  title: "Error",
+                  content: `Phone Number already in use, please use a different number`,
+                  okText: "OK"
+              });
+              return;
+          }
+          await CustomerApi.addCustomer(values);
+          message.success('Customer created successfully!');
+          Modal.confirm({
+            title: "Success",
+            content: "Customer created successfully!",
+            okText: "OK",
+            style: { top: '50%', transform: 'translateY(-50%)' },
+            onOk() {
+              navigate(-1); 
+            }
+          });
+        } catch (error) {
+          message.error('Failed to add customer. Please try again.');
+          Modal.warning({
+            title: "Error",
+            content: `Failed to add customer`,
+            style: { top: '50%', transform: 'translateY(-50%)' },
+            okText: "OK"
+          });
+          console.log('Failed to add customer:', error);
+        }
+      };
 
   return (
     <div className='add-customer-page'>
@@ -29,7 +66,7 @@ const AddCustomerPage = () => {
       <div className="add-customer-form-container">
       <Typography.Title className="add-customer-title" level={3}>Create Profile</Typography.Title>
       <Form form={form} onFinish={onFinish} layout="vertical">
-        <Form.Item name="name" label="Name" rules={[{ required: true }]}>
+        <Form.Item name="customerName" label="Name" rules={[{ required: true }]}>
           <Input />
         </Form.Item>
 
@@ -38,14 +75,29 @@ const AddCustomerPage = () => {
         </Form.Item>
 
         <Form.Item name="gender" label="Gender" rules={[{ required: true }]}>
-          <Input />
+                <Radio.Group>
+                  <Radio value="Male">Male</Radio>
+                  <Radio value="Female">Female</Radio>
+                </Radio.Group>
         </Form.Item>
         
-        <Form.Item name="phoneNumber" label="Phone Number" rules={[{ required: true }]}>
+        <Form.Item name="phoneNumber" label="Phone Number" rules={[
+                { required: true, message: "Please input your Phone number!" },
+                {
+                  pattern: new RegExp(/^[0-9]{10}$/),
+                  message: "Phone number must be exactly 10 digits!",
+                },
+              ]}>
           <Input />
         </Form.Item>
 
-        <Form.Item name="email" label="Email" rules={[{ required: true }]}>
+        <Form.Item name="email" label="Email" rules={[
+                  { required: true, message: "Please input your Email!" },
+                  {
+                    type: 'email',
+                    message: "The input is not valid E-mail!",
+                  },
+                ]}>
           <Input />
         </Form.Item>
 
