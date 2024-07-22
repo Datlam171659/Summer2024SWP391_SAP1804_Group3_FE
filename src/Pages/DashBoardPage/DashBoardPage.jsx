cbef2cdc3c0b01e51aa69eea0c64987681f7c68e
timboace0 <khanglgse171640@fpt.edu.vn>
2024-07-22 14:28:15 +0700

    update

--- src/Pages/DashBoardPage/DashBoardPage.jsx
+++ src/Pages/DashBoardPage/DashBoardPage.jsx
@@ -289,7 +289,7 @@ const DashBoardPage = () => {
             <DashBoardCard icon={<ShoppingCartOutlined style={{ color: "blue" }} />} title={"Đơn Hàng Đã Bán"} value={soldInvoiceCount} key="soldOrders" />
             <DashBoardCard icon={<ShoppingCartOutlined style={{ color: "orange" }} />} title={"Đơn Hàng Đã Mua"} value={buyBackInvoiceCount} key="boughtOrders" />            
             <DashBoardCard icon={<ShoppingOutlined style={{ color: "brown" }} />} title={"Mặt Hàng Bán"} value={soldProductCount} key="soldProducts" />
-            <DashBoardCard icon={<ShoppingOutlined style={{ color: "black" }} />} title={"Mặt Hàng Mua"} value={buyBackProductCount} key="boughtProducts" />
+            <DashBoardCard icon={<ShoppingOutlined style={{ color: "black" }} />} title={"Mặt Hàng Đã Mua"} value={buyBackProductCount} key="boughtProducts" />
             <DashBoardCard icon={<DollarCircleOutlined style={{ color: "green" }} />} title={"Tổng Doanh Thu"} value={formatRevenue(totalRevenue)} key="revenue" />
           </Space>
         </div>
